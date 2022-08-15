02147217ac5826227413b181502981231c51d50a
mizo202208 <mizo@qolt.net>
2022-08-16 07:54:08 +0900

    20200816

--- js/dom.js
+++ js/dom.js
@@ -24,3 +24,16 @@ console.log(document.querySelector('h1'));
 console.log(document.querySelector('#second-heading'));
 console.log(document.querySelector('.list'));
 
+// html要素をCSSセレクタで取得し、中身を出力する（すべて）
+console.log(document.querySelectorAll('.heading'));
+console.log(document.querySelectorAll('li'));
+
+// 新しくli要素を作成し。定数に代入する
+const li = document.createElement('li');
+
+//　作成しli要素にテキストを追加する
+li.textContent = 'javascriptで新しく作成したリスト３';
+
+// ul要素の末尾li要素を追加する
+document.querySelector('ul').appendChild(li);
+
