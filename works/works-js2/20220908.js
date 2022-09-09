09b7e3793a3b071b9475a2740830ebc88bfbf214
mizo202208 <mizo@qolt.net>
2022-09-09 20:52:32 +0900

    20220909-2

---
+++ works/works-js2/20220908.js
@@ -0,0 +1,20 @@
+//課題０５ up-btnというidを持つHTML要素を取得し、定数に代入する
+const upBtn = document.getElementById('up-btn');
+
+//画面がスクロールされたときにイベント処理を実行する
+window.addEventListener('scroll', () => {
+  //画面のスクロール量をpx（ピクセル）数で取得する
+  const scrollvalue = document.scrollingElement.scrollTop;
+
+  //画面がスクロールされると「UP」ボタンを表示する
+  if (scrollvalue >= 1) {
+    upBtn.style.display = 'inline';
+  }
+  //画面のスクロール量がそれ以外（1px未満）であれば、「UP」ボタンを非表示にする
+  else {
+    upBtn.style.display = 'none';
+  }
+  
+})
+
+
