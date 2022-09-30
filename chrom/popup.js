f08cef22023f9a3fdfa6159dc00fdec773338343
mizo202208 <mizo@qolt.net>
2022-10-01 08:40:07 +0900

    20221001

--- chrom/popup.js
+++ chrom/popup.js
@@ -10,7 +10,7 @@ toAddressbtn.addEventListener('click', () => {
   const text = document.forms.textForm.textBox.value
 
   //取得した文字列を出力する
-  alert("アドレス設定");
+  alert("登録アドレス" + text);
 });
    
 // 送金先アドレスを保存
@@ -19,7 +19,7 @@ toAddressbtn.addEventListener('click', () => {
   chrome.storage.local.set ({
     "address":sokinad
   },function (){
-    console.log("登録完了")
+    alert("登録完了")
   })
 
 }
@@ -33,16 +33,16 @@ function load () {
 }
 
 // toknset1というidを持つHTML要素を取得し、定数に代入する
-//const toknSet1 = document.getElementById('toknset1')
+const toknSet1 = document.getElementById('toknset1')
 
 // HTML要素がクリックされたされたときにイベント処理を実行する
-//toknSet1.addEventListener('click', () => {
+toknSet1.addEventListener('click', () => {
   //テキストボックスに入力された文字列を取得する
-  //const tokn1 = document.forms.textForm.textBox.value;
+  const tokn1 = document.forms.textForm.textBox.value;
 
   //取得した文字列を出力する
-  //console.log(tokn1);
-//});
+  console.log(tokn1);
+});
 
 
 document.getElementById('to-addressbtn').addEventListener("click",save)
